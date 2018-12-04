981451116c2359475ca50ae683c4172c41971795
Mason Fidino <mfidino@lpzoo.org>
2018-12-04 11:18:32 -0600

    Update la_river.R

--- la_river.R
+++ la_river.R
@@ -17,3 +17,12 @@ jdata <- read.csv(classifications_file, stringsAsFactors = FALSE)
 
 # parse through the different annotations and workflows
 my_annos <- parse_annotations(jdata)
+
+
+# get the subject id's
+my_photos <- parse_photos(jdata)
+
+# drop NA id
+my_photos <- my_photos[!is.na(my_photos$id),]
+
+# from here you can connect user tags to photos via the classification_id
